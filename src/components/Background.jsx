fec50973e3f2179db49901d170b6fef89ca7b20c
majnioui <majnioui@gmail.com>
2025-11-30 00:53:20 +0100

    wip

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -1,6 +1,9 @@
+import { LightRays } from "@/components/ui/light-rays";
+
 const Background = () => {
     return (
         <div className="fixed inset-0 z-0 overflow-hidden bg-zinc-950">
+            <LightRays />
             <div className="absolute top-[-50%] left-[-50%] w-[200%] h-[200%] opacity-20 animate-pulse">
                 <div className="absolute top-1/2 left-1/2 w-full h-full bg-gradient-to-r from-blue-900/10 to-purple-900/10 blur-[120px] transform -translate-x-1/2 -translate-y-1/2 rounded-full mix-blend-screen" />
             </div>
